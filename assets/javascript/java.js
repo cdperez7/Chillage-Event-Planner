c46d3328099cac48c0e4e6299c6eeb2da5f1b7d7
sujit shah <a.sujitshah34@gmail.com>
2019-04-24 19:43:23 -0500

    newv one

--- assets/javascript/java.js
+++ assets/javascript/java.js
@@ -1,3 +1,17 @@
 $(document).ready(function(){
     $('select').formSelect();
   });
+  function buildQueryURL() {
+
+  }
+  var queryParams = {"api-key": "AIzaSyB8I1jYCzLpdFheT0Fdk2XpdlzpeHJhJwE"};
+  var queryURL = "https://www.google.com/maps/@?api=1&map_action=map";
+  
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  
+  }).then(function(response) {
+    console.log(response);
+    console.log(queryURL);
+  })
